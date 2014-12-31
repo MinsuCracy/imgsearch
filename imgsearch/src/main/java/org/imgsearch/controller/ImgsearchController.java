8ac56658f83e604d6cd732f27454793758765a8f
dobang2 <nodoclass@naver.com>
2014-12-31 16:57:20 +0900

    nodo

--- imgsearch/src/main/java/org/imgsearch/controller/ImgsearchController.java
+++ imgsearch/src/main/java/org/imgsearch/controller/ImgsearchController.java
@@ -1,37 +1,23 @@
 package org.imgsearch.controller;
 
-import java.security.Principal;
-
-import javax.inject.Inject;
-
-import org.imgsearch.service.UserService;
-import org.imgsearch.vo.UserVO;
 import org.imgsearch.web.HomeController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @Controller
-@RequestMapping("/user/*")
+@RequestMapping("/img")
 public class ImgsearchController {
 
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	
-	@RequestMapping("/location")
-	public String index(Principal principal, Model model)throws Exception{
+	@RequestMapping("/index")
+	public void index()throws Exception{
 		
-			logger.info("컨트롤로 입장 location");
-			model.addAttribute("id", principal.getName());
-			logger.info(" 로그인 아이디 : " + principal.getName());
-		return "/user/location";
 	}
-//	@RequestMapping("/insertFriend")
-//	public void insertFriend(UserVO vo)throws Exception{
-//		userService.insertFriend(userService.existFriend(vo));
-//	}
+	
 }
 
